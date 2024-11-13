6fc241c866e87d0f14fba07e22ef64865c79aace
Cauaram0s <00001090934373sp@al.educacao.sp.gov.br>
2024-11-13 08:40:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Vaca é um?',
+    'Mamifero'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'A capital da Argentina é Buenos Aires'
+)
+
+criaCartao(
+    'Cultura',
+    'De onde vem o Açaí?',
+    'Brasil'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Oi em ingles é bye '
+)
